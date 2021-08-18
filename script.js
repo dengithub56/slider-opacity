9be376b5dda6afac0f3b65e5ad327c519c14f432
Korolev Denis <korolev-056@yandex,ru>
2021-08-19 00:56:14 +0500

    js modified add button

--- script.js
+++ script.js
@@ -11,11 +11,44 @@ class Slider {
     constructor(selector, options) {
         this.slider = document.getElementById(selector)
         this.sliderrItems = this.slider.querySelectorAll('img')
-        this.render()
+        this.__render()
+        this.__listener()
+        this.currentIndex = 0
     }
-    render() {
+    __render() {
         this.slider.innerHTML += getTemplate()
     }
+    __listener() {
+        this.nextSlide = this.nextSlide.bind(this)
+        this.prevSlide = this.prevSlide.bind(this)
+
+        this.slider.addEventListener('click', this.nextSlide)
+        this.slider.addEventListener('click', this.prevSlide)
+    }
+    currentSlide(index) {
+        switch (index) {
+            case (index > this.sliderrItems.length):
+                this.currentIndex = 0
+                break
+            case (index < this.sliderrItems.length):
+                this.currentIndex = this.sliderrItems.length
+                break
+            default:
+                this.currentIndex = index
+        }
+        console.log(index)
+    }
+    nextSlide(event) {
+        if (event.target.dataset.type === 'next') {
+            this.currentSlide(this.currentIndex + 1)
+        }
+    }
+    prevSlide(event) {
+        if (event.target.dataset.type === 'prev') {
+            this.currentSlide(this.currentIndex - 1)
+        }
+    }
+
 }
 
 let slider = new Slider('slider-opacity', {})
