07ca74c4f9292bfd40cfd4ac93f85a672065a321
Korolev Denis <korolev-056@yandex,ru>
2021-08-19 01:38:50 +0500

    js refactoring

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Slider {
         this.__render()
         this.__listener()
         this.currentIndex = 0
+        this.slider.querySelectorAll('img')[this.currentIndex].classList.add('show')
     }
     __render() {
         this.slider.innerHTML += getTemplate()
@@ -26,27 +27,29 @@ class Slider {
         this.slider.addEventListener('click', this.prevSlide)
     }
     currentSlide(index) {
-        switch (index) {
-            case (index > this.sliderrItems.length):
-                this.currentIndex = 0
-                break
-            case (index < this.sliderrItems.length):
-                this.currentIndex = this.sliderrItems.length
-                break
-            default:
-                this.currentIndex = index
-        }
-        console.log(index)
+        if (index >= this.sliderrItems.length) this.currentIndex = 0
+        else if (index < 0) this.currentIndex = this.sliderrItems.length - 1
+        else this.currentIndex = index
     }
     nextSlide(event) {
         if (event.target.dataset.type === 'next') {
+            this.removectiveClass(this.currentIndex)
             this.currentSlide(this.currentIndex + 1)
+            this.addActiveClass(this.currentIndex)
         }
     }
     prevSlide(event) {
         if (event.target.dataset.type === 'prev') {
+            this.removectiveClass(this.currentIndex)
             this.currentSlide(this.currentIndex - 1)
+            this.addActiveClass(this.currentIndex)
         }
+    }
+    addActiveClass(index) {
+        this.slider.querySelectorAll('img')[index].classList.add('show')
+    }
+    removectiveClass(index) {
+        this.slider.querySelectorAll('img')[index].classList.remove('show')
     }
 
 }
